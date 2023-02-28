97d2c87ed72b61cddb6bccd946d2d1cb6bf10561
Kunj Choksi <kunj07soni@gmail.com>
2023-02-28 12:53:21 -0500

    added as_json

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,4 +2,14 @@ class Admin < ApplicationRecord
   has_secure_password
 
   enum status: {active: 'active', inactive: 'inactive'}
+
+  def as_json
+    result = Hash.new
+    result['first_name'] = self.first_name
+    result['last_name'] = self.last_name
+    result['email'] = self.email
+    result['status'] = self.status
+
+    result
+  end
 end
