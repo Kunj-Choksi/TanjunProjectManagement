2e8bac3e79d1b8589def30ae413dfb9f9ee0944a
Kunj Choksi <kunj07soni@gmail.com>
2023-02-28 19:45:34 -0500

    Added admin unit test

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,9 +1,13 @@
 class Admin < ApplicationRecord
   has_secure_password
-  has_many :developers, class_name: "Developer", foreign_key: :created_by_id
-  has_many :projects, class_name: "Project", foreign_key: :created_by_id
+  has_many :developers, class_name: 'Developer', foreign_key: :created_by_id
+  has_many :projects, class_name: 'Project', foreign_key: :created_by_id
 
   enum status: { active: 'active', inactive: 'inactive' }
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: /\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/
 
   def as_json
     result = {}
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,22 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  admin1 = Admin.new
-
-  it 'should not be valid with no params' do
-    expect(admin1).not_to be_valid
+  before do
+    @admin1 = Admin.create(first_name: "Test", last_name: "LName", email: "test@gmail.com", password: "sompassword", password_confirmation: "sompassword")
   end
 
-  admin2 = described_class.new(
-    "first_name": 'Kunj',
-    "last_name": 'Choksi',
-    "email": 'kunj07soni@gmail.com',
-    "password": 'password123',
-    "password_confirmation": 'password123'
-  )
+  describe "model validation" do
+    it "should be valid with attrs" do
+      expect(@admin1).to be_valid
+    end
 
-  it 'should have default status of inactive' do
-    expect(admin2.status).to eq('inactive')
-    expect(admin2.status).not_to eq('pending')
+    it "should not valid w/o first name" do
+      @admin1.first_name = nil
+      expect(@admin1).to be_invalid
+    end
+
+    it "should not valid w/o last name" do
+      @admin1.last_name = nil
+      expect(@admin1).to be_invalid
+    end
+
+    it "should have a unique email" do
+      @admin2 = Admin.create(first_name: "Test", last_name: "LName", email: "test@gmail.com", password: "sompassword", password_confirmation: "sompassword")
+
+      expect(@admin2).to be_invalid
+    end
+
+    it "should have a valid email format" do
+      @admin3 = Admin.create(first_name: "Test", last_name: "LName", email: "testail.com", password: "sompassword", password_confirmation: "sompassword")
+
+      expect(@admin3).to be_invalid
+    end
+
+    it "should have a default status of inactive" do
+      expect(@admin1.status).to eq('inactive')
+    end
   end
 end
